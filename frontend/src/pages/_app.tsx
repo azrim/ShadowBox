c91901851eccbc03df780e9098e782f7f45c663d
azrim <mirzaspc@gmail.com>
2025-11-18 13:12:42 +0700

    frontend: add Vercel Speed Insights

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -8,6 +8,7 @@ import { useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 import { createAppKit } from "@reown/appkit/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { config, wagmiAdapter, projectId, networks } from "@/lib/wagmi";
 
 const queryClient = new QueryClient();
@@ -45,7 +46,10 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        <>
+          <Component {...pageProps} />
+          <SpeedInsights />
+        </>
       </QueryClientProvider>
     </WagmiProvider>
   );
